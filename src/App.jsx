3abd899f25359f4e0d0fe568f686ae2e4a196930
Marc.A <marcalberopascual@gmail.com>
2023-11-22 19:42:07 +0100

    adding to cart and choosing size

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react'
 import './App.css'
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import products from "../products.json";
 import Product from "./components/Product";
 
 function App() {
   const [selectedProduct, setSelectedProduct] = useState(products[0])
+  
+  const [cart, setCart] = useState([]);
 
+  const addToCart =(productToAdd)=>{
+    const cartCopy = [...cart];
+    cartCopy.push(productToAdd);
+    setSelectedProduct(cartCopy);
+  }
+  
   return (
     <>
       <div>
-       
+       <Navbar />
         <div className="content">
-          <Product selectedProduct={selectedProduct}></Product>
+          <Product selectedProduct={selectedProduct} addToCart={addToCart}></Product>
+        {/*<Gallery selectedProduct={selectedProduct} setSelectedProduct={setSelectedProduct} />*/}
         </div>
-        
+        <Footer />
        
       </div>
     </>

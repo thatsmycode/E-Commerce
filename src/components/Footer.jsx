b8c6ea38e2373d04b3c3d02f225bb64e5ca2a4de
Victor <victor.castanos@gmail.com>
2023-11-22 17:40:47 +0100

    footer.jsx jooter.css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+<footer id="footer">
+    <div id="footer-div1">
+<img  src="../public/images/planetaria-logo.png" alt="planetaria-logo" />
+<p>Planetaria Music</p>
+    </div>
+    <div id="footer-div2">
+<img src="../public/images/social-networks.png" alt="social-networks" />
+    </div>
+</footer>
+
+)}

b7dba820d99c186e5df51119abe22cb0f6137339
Martin Palkov <martin.palkov@gmail.com>
2023-11-22 17:00:03 +0100

    add components Navbar GAllery Footer into the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Gallery from './components/Gallery';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,10 +10,12 @@ function App() {
   return (
     <>
       <div>
-       
+        <Navbar />
+        <Gallery />
+        <Footer />
       </div>
     </>
   )
 }
 
-export default App
+export default App;

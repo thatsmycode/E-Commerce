aee5335f88ebab3df709a180ddb027c2f9b80bad
Marc.A <marcalberopascual@gmail.com>
2023-11-22 17:13:21 +0100

    images and json added

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react";
+import products from "../"
+
+const Gallery = () => {
+    const [display, setDisplay] = ();
+
+    return(
+        {}
+    )
+}
+export default Gallery;

65c34d5a5c7d95fd6088a726f0b55686cfd4cb10
Marc.A <marcalberopascual@gmail.com>
2023-11-23 17:04:35 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import products from "../products.json";
 import Product from "./components/Product";
 import Gallery from './components/Gallery';
 import { Routes, Route, Navigate} from "react-router-dom";
-
+import Home from "./components/Home";
 
 function App() {
   const [selectedProduct, setSelectedProduct] = useState(products[0])
@@ -25,9 +25,10 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Navigate to="/store"/>} />
+            <Route path="/" element={<Home/>} />
             <Route path="/store" element={<Gallery selectedProduct={selectedProduct} setSelectedProduct={setSelectedProduct} />} />
             <Route path="/store/product-details" element={<Product selectedProduct={selectedProduct} addToCart={addToCart} />} />
+            <Route path="*" element={<Navigate to="/store"/>} />
           </Routes>
         </div>
         <Footer />
